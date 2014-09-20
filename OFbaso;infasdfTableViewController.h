3247f4572abfc6783e1d68e3ab2a5195c9ad782d
andrew <afriday11@gmail.com>
2014-09-20 10:35:49 +0800

    added another class

---
+++ OFbaso;infasdfTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  OFbaso;infasdfTableViewController.h
+//  git test
+//
+//  Created by Andrew Friday on 9/20/14.
+//  Copyright (c) 2014 Andrew Friday. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OFbaso_infasdfTableViewController : UITableViewController
+
+@end
